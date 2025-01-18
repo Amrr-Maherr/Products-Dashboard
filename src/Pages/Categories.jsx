6f3acbde006a240d9533c6bd8836b5ebb7b31369
Amr maher <amrm80233@>
2025-01-18 12:52:30 +0200

    fix: Update Categories component to fetch and display categories
    - Implemented API call to fetch categories from the server using axios.
    - Fixed category data state management to handle loading and errors.
    - Displayed categories list (currently not shown in the component but ready for future display).
    - Added basic structure for handling the loading state and API errors.

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -1,4 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function Categories() {
+  const [categories, ssetCategories] = useState([])
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5003/categories")
+      .then((response) => {
+        console.log(response.data);
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(true)
+      });
+  },[])
     return (
       <>
         <h1>Categories</h1>
