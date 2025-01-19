1b07aab0b4a8eb4340b35c67de7b55f56230751d
Amr maher <amrm80233@>
2025-01-19 10:05:17 +0200

    change navbar link potion to the right and add some shadow

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
         <div className="container">
-          <Link className="navbar-brand me-auto" to="/">
+          <Link className="navbar-brand ms-auto" to="/">
             Home
           </Link>
         </div>
